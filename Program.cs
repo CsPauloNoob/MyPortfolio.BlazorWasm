d0333d27996a7f20c18a42f2ea2b39ec5fb99af0
CsPauloNoob <ps616131@gmail.com>
2025-01-05 18:09:24 -0500

    Centralizado todo o conteúdo do curriculo na página index

--- Program.cs
+++ Program.cs
@@ -2,13 +2,16 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MyPortfolio.BlazorWasm;
 using MyPortfolio.BlazorWasm.Extensions;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
 //injecao de dependencias
 builder.Services.injectDependences();
+builder.Services.AddMudServices();
 
 builder.Services.
     AddHttpClient(WebConfiguration.Name,
