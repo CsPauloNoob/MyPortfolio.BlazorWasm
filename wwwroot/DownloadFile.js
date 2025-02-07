8533ec7a7bd425b597d2ce5567b5c2cc805e8354
paulo sergio <ps616131@gmail.com>
2025-02-06 21:27:56 -0500

    implementada feature

--- wwwroot/DownloadFile.js
+++
@@ -1,6 +0,0 @@
-async function downloadFileFromStream(filename, byteBase64) {
-    var link = document.createElement('a');
-    link.href = 'data:application/pdf;base64,' + byteBase64;
-    link.download = filename;
-    link.click();
-}
